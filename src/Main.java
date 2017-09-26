c195527e07a33103538f3975a3aa344263b231d7
tomneozed <tomneozed@gmail.com>
2017-09-26 09:42:29 +0200

    v1

---
+++ src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class Main 
+{
+
+	public static void main(String[] args) 
+	{
+		menu();
+		
+	}
+	
+	public static void menu()
+	{
+		int choixMenu = 0;
+		Scanner scan = new Scanner(System.in);
+		
+		do
+		{
+			System.out.println("\n\n************************* MENU *************************");
+			System.out.println("\t1. Recherche +/-");
+			System.out.println("\t2. Mastermind");
+			System.out.println("\t3. Quitter");
+			System.out.println("\nVotre choix : ");
+			
+			choixMenu = scan.nextInt();
+			
+			switch(choixMenu)
+			{
+			case 1 :
+				RecherchePlusMoins rpm = new RecherchePlusMoins();
+				rpm.choisirMode();
+				break;
+				
+			case 3 :
+				break;
+			default :
+				break;
+				
+			}
+		}while (choixMenu != 3);
+		
+		System.out.println("Au revoir ! :)");
+	}
+}
